b6cdc829da26802834e915c6b9bd7000ce6986d4
Thomas McMahon <tmcmahon@nerdery.com>
2019-04-16 13:14:42 -0500

    Updating Version Number

--- pdfjs-viewer.php
+++ pdfjs-viewer.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: PDFjs Viewer
 Plugin URI: http://byterevel.com/
-Description: Embed PDFs with the gorgeous PDF.js viewer
-Version: 1.3
+Description: Embed PDFs with the gorgeous PDF.js viewer - Forked by Thomas McMahon
+Version: 1.4
 Author: Ben Lawson
 Author URI: http://byterevel.com/
 License: GPLv2
@@ -16,9 +16,9 @@ License: GPLv2
 add_shortcode("pdfjs-viewer", "pdfjs_handler");
 
 function pdfjs_handler($incoming_from_post) {
-  //set defaults 
+  //set defaults
   $incoming_from_post=shortcode_atts(array(
-    'url' => 'bad-url.pdf',  
+    'url' => 'bad-url.pdf',
     'viewer_height' => '1360px',
     'viewer_width' => '100%',
     'fullscreen' => 'true',
@@ -35,7 +35,7 @@ function pdfjs_handler($incoming_from_post) {
 
 function pdfjs_generator($incoming_from_handler) {
   $viewer_base_url= plugins_url()."/pdfjs-viewer-shortcode/pdfjs/web/viewer.html";
-  
+
 
   $file_name = $incoming_from_handler["url"];
   $viewer_height = $incoming_from_handler["viewer_height"];
@@ -44,27 +44,27 @@ function pdfjs_generator($incoming_from_handler) {
   $download = $incoming_from_handler["download"];
   $print = $incoming_from_handler["print"];
   $openfile = $incoming_from_handler["openfile"];
-  
+
   if ($download != 'true') {
       $download = 'false';
   }
-  
+
   if ($print != 'true') {
       $print = 'false';
   }
-  
+
   if ($openfile != 'true') {
       $openfile = 'false';
   }
-  
+
   $final_url = $viewer_base_url."?file=".$file_name."&download=".$download."&print=".$print."&openfile=".$openfile;
-  
+
   $fullscreen_link = '';
   if($fullscreen == 'true'){
        $fullscreen_link = '<a href="'.$final_url.'">View Fullscreen</a><br>';
   }
   $iframe_code = '<iframe width="'.$viewer_width.'" height="'.$viewer_height.'" src="'.$final_url.'"></iframe> ';
-  
+
   return $fullscreen_link.$iframe_code;
 }
 
